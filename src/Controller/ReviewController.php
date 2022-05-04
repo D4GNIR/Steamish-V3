49ff655835816366cf2e4a8107b5dea07f79a309
Mattroc <93247451+Mattroc@users.noreply.github.com>
2022-05-04 12:19:37 +0200

    Maj Homepage

---
+++ src/Controller/ReviewController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReviewController extends AbstractController
+{
+    #[Route('/review', name: 'app_review')]
+    public function index(): Response
+    {
+        return $this->render('review/review.html.twig', [
+            'controller_name' => 'ReviewController',
+        ]);
+    }
+}
