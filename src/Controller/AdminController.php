2e7eab896c2130daaeb234619f80aa3f20763bd5
D4GNIR <jeremiecette@gmail.com>
2022-05-09 14:21:33 +0200

    working on crud genre

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,42 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin/dashboard/genre', name: 'app_adminGenre')]
+    public function indexGenre(GenreRepository $genreRepository): Response
+    {
+        $genre = $genreRepository->findAll();
+        return $this->render('admin/indexGenre.html.twig', [
+            'genres' => $genre
+        ]);
+    }
+
+    #[Route('/admin/dashboard/genre/show/{slug}', name: 'app_genre_show')]
+    public function showGenre(GenreRepository $genreRepository): Response
+    {
+        $genre = $genreRepository->findAll();
+        return $this->render('admin/indexGenre.html.twig', [
+            'genres' => $genre
+        ]);
+    }
+
+    #[Route('/admin/dashboard/genre/edit/{slug}', name: 'app_genre_edit')]
+    public function editGenre(GenreRepository $genreRepository): Response
+    {
+        $genre = $genreRepository->findAll();
+        return $this->render('admin/indexGenre.html.twig', [
+            'genres' => $genre
+        ]);
+    }
+
+    #[Route('/admin/dashboard/genre/delete/{slug}', name: 'app_genre_delete')]
+    public function deleteGenre(GenreRepository $genreRepository): Response
+    {
+        $genre = $genreRepository->findAll();
+        return $this->render('admin/indexGenre.html.twig', [
+            'genres' => $genre
         ]);
     }
 }
