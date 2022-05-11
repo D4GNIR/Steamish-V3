aefd2edc833349c2299eb301c3915e1bc8af3f30
D4GNIR <jeremiecette@gmail.com>
2022-05-11 14:53:14 +0200

    CRUD game ok

--- src/Controller/AdminGameController.php
+++ src/Controller/AdminGameController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Form\AddGameType;
 use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminGameController extends AbstractController
 {
+    private GameRepository $gameRepository;
+    private PaginatorInterface $paginator;
+    private SluggerInterface $sluger;
+
     public function __construct(
-        private GameRepository $gameRepository,
-        private PaginatorInterface $paginator
+        GameRepository $gameRepository,
+        PaginatorInterface $paginator,
+        SluggerInterface $sluger
     ) {
         $this->gameRepository = $gameRepository;
         $this->paginator = $paginator;
+        $this->sluger = $sluger;
      }
 
     #[Route('/admin/game_liste', name: 'app_admin_game')]
@@ -45,5 +55,56 @@ class AdminGameController extends AbstractController
         return $this->render('admin_game/show.html.twig', [
             'game' => $game
         ]);
+    }
+
+    #[Route('/admin/games/create', name: 'app_game_create')]
+    public function addGame(GameRepository $gameRepository,Request $request,EntityManagerInterface $em): Response
+    {
+        $gameEntity = new Game();
+        $form = $this->createForm(AddGameType::class, $gameEntity);
+
+        // Cette ligne permet le lien entre formulaire et entité si le formulaire est lié à l'entité
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $this->sluger->slug($gameEntity->getName());
+            $gameEntity->setSlug($slug);
+            $em->persist($gameEntity);
+            $em->flush();
+            return $this->redirectToRoute('app_admin_game');
+        }
+
+        return $this->render('admin_game/addGame.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/games/edit/{slug}', name: 'app_game_edit')]
+    public function editGenre(Game $game, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(AddGameType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = $this->sluger->slug($game->getName());
+            $game->setSlug($slug);
+
+            $em->flush();
+            return $this->redirectToRoute('app_admin_game');
+        }
+
+        return $this->render('admin_game/addGame.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/games/delete/{slug}', name: 'app_game_delete')]
+    public function deleteGame(Game $game, Request $request, EntityManagerInterface $em): Response
+    {
+
+            $em->remove($game);
+            $em->flush();
+            return $this->redirectToRoute('app_admin_game');
+
     }
 }
